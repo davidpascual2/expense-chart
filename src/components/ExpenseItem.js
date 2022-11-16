41c382988b3fc121fee0b8fbf3f7add56b9f4e3d
David Pascual <David@Dairas-MacBook-Air.local>
2022-11-15 19:07:23 -0800

    added expenseDate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,15 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
     //in regular js we use parameters to pass data into functions 
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.date.getFullYear();
+    // const month = props.date.toLocaleString('en-US', {month: 'long'});
+    // const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    // const year = props.date.getFullYear();
     
     return (
         <div className='expense-item'>
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date} />
             <div className='expense-item-desc'>
                 <h2>{props.title}</h2>
                 <div className='expense-item-price'>${props.amount}</div>
